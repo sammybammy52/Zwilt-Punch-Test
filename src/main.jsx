5fc7d2422b13d05dc99f49483e6e28c8c62eb59d
Seye Yemi-Olowolabi <sammybammystudios@gmail.com>
2024-06-03 10:49:43 +0100

    remove chakra, didnt use it

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { ChakraProvider } from "@chakra-ui/react";
 import { DataProvider } from "./context/DataContext";
 import { AuthProvider } from "./context/AuthContext";
 
@@ -10,9 +9,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AuthProvider>
       <DataProvider>
-        <ChakraProvider>
           <App />
-        </ChakraProvider>
       </DataProvider>
     </AuthProvider>
   </React.StrictMode>
